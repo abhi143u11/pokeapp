2b168b89864918f9f62a9c1b28d159741114930c
Abhishek Jain <abhisheknjain11@gmail.com>
2020-04-03 20:42:25 +0530

    Initial Commit

--- lib/constants.dart
+++
@@ -1,3 +0,0 @@
-const kUrl = "http://192.168.0.108/acsphp/";
-const kAllCategories = "allcat.php";
-const kAllServices = "allservices.php";
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pokeapp/result.dart';
-
-import 'quiz.dart';
 
 //
 //     final hitsArray = hitsArrayFromJson(jsonString);
@@ -14,72 +11,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final _questions = const [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': [
-          {'text': 'Black', 'score': 10},
-          {'text': 'Red', 'score': 5},
-          {'text': 'Green', 'score': 3},
-          {'text': 'White', 'score': 1},
-        ],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': [
-          {'text': 'Rabbit', 'score': 3},
-          {'text': 'Snake', 'score': 11},
-          {'text': 'Elephant', 'score': 5},
-          {'text': 'Lion', 'score': 9},
-        ],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': [
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-        ],
-      },
-    ];
-
-    void _answerQuestion(int score) {
-      _totalScore = _totalScore + score;
-      print(_totalScore);
-      setState(() {
-        _questionIndex = _questionIndex + 1;
-      });
-      print(_questionIndex);
-    }
-
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First App'),
+        ),
+        body: Container(),
       ),
-      body: _questionIndex < _questions.length
-          ? Quiz(
-              answerQuestion: _answerQuestion,
-              questionIndex: _questionIndex,
-              questions: _questions,
-            )
-          : Result(
-              resultScore: _totalScore,
-              resetHandler: _resetQuiz,
-            ),
-    ));
+    );
   }
 }
