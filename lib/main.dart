eefb7c46dd9766d4968adc2237d48669dadb0c29
Abhishek Jain <abhisheknjain11@gmail.com>
2020-04-03 22:51:14 +0530

    Completed till adding Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-//
+import 'widget/user_transactions.dart';
 //     final hitsArray = hitsArrayFromJson(jsonString);
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +16,17 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('First App'),
         ),
-        body: Container(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              color: Colors.blue,
+              child: Text('Chart'),
+            ),
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }
